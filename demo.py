bdc7cd5b0d661ebb8c37373dfba233282e7aa493
Paulus Kutrich <pkutrich@gmail.com>
2019-09-12 15:21:30 -0700

    Add iterable feature to demo

--- demo.py
+++ demo.py
@@ -3,22 +3,27 @@ from Trie import Trie
 
 def main():
     """A quick demo of Trie insert and list_words methods."""
-    print(f'Add 466551 words to the Trie...')
 
     # Instantiate an empty Trie.
     trie = Trie()
-    with open('words.txt', 'r') as f:
-        text = f.readlines()
-    # Add a bunch of words.
-    for word in text:
-        trie.insert(word.strip())
 
-    print('Find words starting with:')
-    print(f"worm: {trie.list_words('worm')}")
-    print(f"corp: {trie.list_words('corp')}")
-    print(f"strap: {trie.list_words('strap')}")
-    print(f"thing: {trie.list_words('thing')}")
-    print(f"basec: {trie.list_words('basec')}")
+    # Add individual words.
+    trie.insert('word')
+
+    # Add phrases.
+    trie.insert('Any old phrase, any length.')
+    trie.insert('will do.')
+
+    # Add iterables.
+    print(f'Add 466551 words to the Trie...')
+    with open('words.txt', 'r') as f:
+        text = [word.strip() for word in f.readlines()]
+    trie.insert(text)
+
+    print('Find strings starting with:')
+    print(f"any: {trie.list_words('any')}")
+    print(f"prefix: {trie.list_words('prefix')}")
+    print(f"will do: {trie.list_words('will do')}")
 
 
 if __name__ == "__main__":
